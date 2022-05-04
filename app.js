be68744796c317ed3d528944b654b25bc0d91e95
Peter Ugochukwu Uche <pjgraphixs@gmail.com>
2022-05-04 17:54:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const openModal = document.querySelector(".thankContainer");
 const starNumber = document.querySelectorAll(".numberButton");
-const starValue = document.querySelector(".starValue");
+var numberSelected = document.getElementById("numberSelected");
+var numberRating = 0;
 
-
-starNumber.forEach((numberButton, index)=>{
-    numberButton.addEventListener("click", ()=>{
-        starNumber.forEach((numberButton)=>{
-        numberButton.style.backgroundColor = "hsl(212, 19%, 26%)";
-        numberButton.style.color="hsl(217, 12%, 63%)";
-        });
-        numberButton.style.backgroundColor = "hsl(217, 12%, 63%)";
-        numberButton.style.color="white";
-    });
+starNumber.forEach(function(e){
+    e.addEventListener("click", clickNumber);
 });
-
+function clickNumber(){
+    starNumber.forEach(function(e){
+        e.id = false;
+    });
+    numberRating = this.innerHTML;
+    this.id = "active";
+}
 function submit(){
     openModal.style.display="block";
+    numberSelected.innerHTML = numberRating;
 }
