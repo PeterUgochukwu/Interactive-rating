0dac2f69155be3cd9964bfd909c2395189e69567
Peter Ugochukwu Uche <pjgraphixs@gmail.com>
2022-05-04 18:00:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const starNumber = document.querySelectorAll(".numberButton");
 var numberSelected = document.getElementById("numberSelected");
 var numberRating = 0;
 
+
+/*starNumber.forEach((numberButton, index)=>{
+    numberButton.addEventListener("click", ()=>{
+        starNumber.forEach((numberButton)=>{
+        numberButton.style.backgroundColor = "hsl(212, 19%, 26%)";
+        numberButton.style.color="hsl(217, 12%, 63%)";
+        });
+        numberButton.style.backgroundColor = "hsl(217, 12%, 63%)";
+        numberButton.style.color="white";
+    });
+}); */
 starNumber.forEach(function(e){
     e.addEventListener("click", clickNumber);
 });
